976e9793ff3cff65b2f07be04ee582901bab7327
Zhenyi Wang <wangzy17@tsinghua.org.cn>
2025-04-30 10:21:26 +0800

    update

---
+++ Codes/main_hmln.py
@@ -0,0 +1,23 @@
+from embedding import load_graph_network
+from train import fit_transform
+import torch
+import numpy as np
+from scipy.sparse import identity, csr_matrix
+
+if __name__=="__main__":
+    torch.cuda.empty_cache()  # 清空缓存
+    torch.backends.cuda.max_split_size_mb = 128  # 减少内存碎片
+    relationship_file = '/home/llma/wzy/UKB_net/GLIM-main/data/relationship_table_20250427.txt'
+    node_feature_file = '/home/llma/wzy/UKB_net/GLIM-main/data/node_feature_20250427.npy'
+    embedding_save_file = '/home/llma/wzy/UKB_net/GLIM-main/results/hmln_feature_20250427.npy'
+    # relationship_file = '/home/llma/wzy/comorbidity/Data/merged_df_long.txt'
+    # node_feature_file = '/home/llma/wzy/comorbidity/Data/UKB_node_feature.npy'
+    # embedding_save_file = '/home/llma/wzy/comorbidity/Data/UKB_feature.npy'
+    
+    features, adj = load_graph_network(relationship_file, node_feature_file)
+    
+    #adj = adj + np.eye(adj.shape[0])
+    # 使用稀疏矩阵的方式添加自环
+    adj = adj + identity(adj.shape[0], dtype=adj.dtype, format='csr')
+    
+    fit_transform(features, adj, embedding_save_file, device='cuda', tmp_path='hmln_best_dgi.pkl')
--- Codes/train.py
+++ Codes/train.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy.sparse import issparse
 import torch_geometric
 from torch_geometric.data import Data
-from torch_geometric.loader import NeighborLoader
+from torch_geometric.loader import NeighborLoader, ClusterData, ClusterLoader, NeighborSampler
 
 # def fit_transform(features, 
 #                   adj, 
@@ -80,7 +80,7 @@ from torch_geometric.loader import NeighborLoader
     
 #     return embeds
 
-## Neighbor sampling
+## Batch
 def fit_transform(features, 
                   adj, 
                   save_path, 
@@ -90,69 +90,107 @@ def fit_transform(features,
                   patience=50, 
                   n_epoch=300, 
                   tmp_path='best_dgi.pkl'):
+    
     device = torch.device(device)
     
     # Self loop is required.
     # 处理稀疏矩阵获取边索引
     if issparse(adj):
-        edge_index = torch.LongTensor(np.column_stack(adj.nonzero())).T.to(device)
+        edge_index = torch.LongTensor(np.column_stack(adj.nonzero())).T
     else:
-        edge_index = torch.LongTensor(np.where(adj > 0)).to(device)
+        edge_index = torch.LongTensor(np.where(adj > 0))
     
     # 确保 edge_index 是连续的
     edge_index = edge_index.contiguous()
     # Features_shape = n_sample * n_dim
     if isinstance(features, np.ndarray):
-        features = torch.FloatTensor(features).to(device)
+        features = torch.FloatTensor(features)
     elif issparse(features):
-        features = torch.FloatTensor(features.toarray()).to(device)
+        features = torch.FloatTensor(features.toarray())
 
      # 确保 features 是连续的
     features = features.contiguous()
     hid_units = n_hid
     feature_size = features.shape[1]
     
-    summary = models.Summary('max').to(device)
-    encoder = models.Encoder(feature_size, hid_units).to(device)
+    summary = models.Summary('max')
+    encoder = models.Encoder(feature_size, hid_units)
     corruption = models.corruption
-    model = models.GraphLocalInfomax(hid_units, encoder, summary, corruption).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    model = models.GraphLocalInfomax(hid_units, encoder, summary, corruption)
+    model = model.to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
+
+    # # train for neighborLoader
+    # def train():
+    #     model.train()
+    #     total_loss = 0
+    #     for batch in loader:
+    #         optimizer.zero_grad()
+    #         #print(f"Min node ID: {batch.n_id.min()}, Max node ID: {batch.n_id.max()}, Features shape: {features.shape}")
+    #         batch_features = features[batch.n_id].to(device)
+    #         # 确保 batch_features 是连续的
+    #         batch_features = batch_features.contiguous()
+    #         batch_edge_index = batch.edge_index.to(device)
+    #         # 确保 batch_edge_index 是连续的
+    #         batch_edge_index = batch_edge_index.contiguous()
+    #         #print(f"Batch features shape: {batch_features.shape}")
+    #         #print(f"batch_edge_index.shape: {batch_edge_index.shape}")
+    #         #print(f"Minimum node ID in edge_index: {torch.min(batch_edge_index)}")
+    #         #print(f"Maximum node ID in edge_index: {torch.max(batch_edge_index)}")
+    #         #print(f"Original node IDs len: {len(batch.n_id)}")
+    #         pos_z, neg_z, summary = model(batch_features, batch_edge_index)
+    #         loss = model.loss(pos_z, neg_z, summary)
+    #         loss.backward()
+    #         optimizer.step()
+    #         total_loss += loss.item()
+    #         del batch_features, batch_edge_index
+    #         #print('one batch!')
+    #     return total_loss / len(loader)
+
+     # 创建 NeighborLoader
+    data = torch_geometric.data.Data(x=features, edge_index=edge_index)
+    data.n_id = torch.arange(data.num_nodes)
+
+    features = features.to(device)
+    features = features.contiguous()
     
+    # loader = NeighborLoader(
+    #     data,
+    #     num_neighbors=[2000,2000],  # 每层采样的邻居数量
+    #     batch_size=5,  # 批量大小
+    #     shuffle=True
+    # )
+
+    ## Cluster-GCN
+    cluster_data = ClusterData(data, num_parts=1500, recursive=False, save_dir=None)
+    train_loader = ClusterLoader(cluster_data, batch_size=100, shuffle=True, num_workers=0)
+    #subgraph_loader = NeighborSampler(data.edge_index, sizes=[-1], batch_size=1024, shuffle=False, num_workers=12)
+    
+    # train for Cluster-GCN
     def train():
         model.train()
         total_loss = 0
-        for batch in loader:
+        total_nodes = 0
+        for batch in train_loader:
+            batch = batch.to(device)
             optimizer.zero_grad()
-            #print(f"Min node ID: {batch.n_id.min()}, Max node ID: {batch.n_id.max()}, Features shape: {features.shape}")
-            batch_features = features[batch.n_id].to(device)
+
+            batch_features = features[batch.n_id]
             # 确保 batch_features 是连续的
             batch_features = batch_features.contiguous()
-            batch_edge_index = batch.edge_index.to(device)
+            batch_edge_index = batch.edge_index
             # 确保 batch_edge_index 是连续的
             batch_edge_index = batch_edge_index.contiguous()
-            #print(f"Batch features shape: {batch_features.shape}")
-            #print(f"batch_edge_index.shape: {batch_edge_index.shape}")
-            #print(f"Minimum node ID in edge_index: {torch.min(batch_edge_index)}")
-            #print(f"Maximum node ID in edge_index: {torch.max(batch_edge_index)}")
-            #print(f"Original node IDs len: {len(batch.n_id)}")
             pos_z, neg_z, summary = model(batch_features, batch_edge_index)
             loss = model.loss(pos_z, neg_z, summary)
             loss.backward()
             optimizer.step()
-            total_loss += loss.item()
+            nodes = batch.n_id.size(0)
+            total_nodes += nodes
+            total_loss += loss.item()*nodes
             del batch_features, batch_edge_index
-            #print('one batch!')
-        return total_loss / len(loader)
-    
-    # 创建 NeighborLoader
-    data = torch_geometric.data.Data(x=features, edge_index=edge_index)
-    data.n_id = torch.arange(data.num_nodes)
-    loader = NeighborLoader(
-        data,
-        num_neighbors=[100,100],  # 每层采样的邻居数量
-        batch_size=50,  # 批量大小
-        shuffle=True
-    )
+        print('Total nodes: {:03d}'.format(total_nodes))
+        return total_loss / total_nodes
     
     best = 1e9
     for epoch in range(1, n_epoch+1):
@@ -178,7 +216,7 @@ def fit_transform(features,
     loader_for_embeds = NeighborLoader(
         data,
         num_neighbors=[-1],  # all neighbors
-        batch_size=50,  # 批量大小
+        batch_size=500,  # 批量大小
         shuffle=False
     )
     all_embeds = []
@@ -186,9 +224,9 @@ def fit_transform(features,
         model.eval()
         for batch in loader_for_embeds:
             batch_features = features[batch.n_id].to(device)
-            batch_features = batch_features.contiguous()
+            #batch_features = batch_features.contiguous()
             batch_edge_index = batch.edge_index.to(device)
-            batch_edge_index = batch_edge_index.contiguous()
+            #batch_edge_index = batch_edge_index.contiguous()
             batch_embeds, _, _ = model(batch_features, batch_edge_index)
             # only retain top batch_size embeds
             batch_embeds = batch_embeds[:batch.batch_size].detach().cpu().numpy()
